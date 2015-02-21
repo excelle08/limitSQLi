7018619e3f9cad32d3f56ff52a06575e2b8ab98c
Excelle Su <2014060104018@std.uestc.edu.cn>
2015-02-21 13:44:06 +0800

    Update
    Correction..

--- ignored.py
+++ ignored.py
@@ -6,6 +6,6 @@ IGNORED_DBS = ['information_schema', 'performance_schema', 'mysql']
 def isIgnorable(name):
     result = False
     for val in IGNORED_DBS:
-        if val == name.tolower():
+        if val == name.lower():
             result = False
     return result
